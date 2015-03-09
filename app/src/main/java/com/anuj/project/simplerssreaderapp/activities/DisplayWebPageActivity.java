e4fa564c8d1d75b114fb423928dd57a8efd0476a
Anuj Kulkarni <anuj@ccs.neu.edu>
2015-03-09 01:08:44 -0400

    Moved files from src to app for android 5.0+

---
+++ app/src/main/java/com/anuj/project/simplerssreaderapp/activities/DisplayWebPageActivity.java
@@ -0,0 +1,50 @@
+package com.anuj.project.simplerssreaderapp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.anuj.project.simplerssreaderapp.R;
+
+/**
+ * Created by kulkan02 on 009-09-03-15.
+ */
+public class DisplayWebPageActivity extends Activity {
+    WebView webview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        Intent in = getIntent();
+        String page_url = in.getStringExtra("page_url");
+
+        webview = (WebView) findViewById(R.id.webpage);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(page_url);
+
+        webview.setWebViewClient(new DisPlayWebPageActivityClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class DisPlayWebPageActivityClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
